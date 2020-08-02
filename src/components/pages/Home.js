ce315ed5befdb25e2dcf24de5149c02f61fa053b
Shelby Cook <shelby.elle.cook@gmail.com>
2020-08-02 13:33:57 -0500

    moviesReducer array is rendering on the home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,1 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+class Home extends Component {
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'GET_ALL_MOVIES',
+    });
+  }
+
+  clickMovie = (event, id) => {
+    this.props.history.push(`/details/${id}`);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        {this.props.store.moviesReducer.map((item, index) => (
+          <div
+            key={index}
+            className="listItem"
+            onClick={(event) => this.clickMovie(event, item.id)}
+          >
+            <img src={item.poster} alt={item.title} />
+            <div>
+              <h5>{item.title}</h5>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStoreToProps = (store) => {
+  return {
+    store,
+  };
+};
+
+export default connect(mapStoreToProps)(Home);
