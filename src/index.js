230f02d76829d99efde3ba22e5012107721b3bc6
Shelby Cook <shelby.elle.cook@gmail.com>
2020-08-02 12:55:26 -0500

    editing saga names for clarity and accuracy

--- src/index.js
+++ src/index.js
@@ -9,20 +9,25 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
+import { takeLatest, put } from 'redux-saga/effects'; //latest not every
 import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
-  yield takeEvery('GET_MOVIES', getMoviesSaga);
+  yield takeLatest('GET_ALL_MOVIES', getAllMoviesSaga);
+  yield takeLatest('GET_MOVIE', getMovieSaga);
+  yield takeLatest('PUT_MOVIE', putMovieDetailsSaga);
+  yield takeLatest('GET_MOVIE_GENRES', getMovieGenresSaga);
+
   //   yield takeEvery('POST_MOVIES', postMoviesSaga);
 }
 
-function* getMoviesSaga(action) {
+// SAGAS
+function* getAllMoviesSaga(action) {
   try {
     const response = yield axios.get('/movies');
     yield put({
-      type: 'POST_MOVIES',
+      type: 'SET_MOVIES', //changed from POST because it was condusing
       payload: response.data,
     });
   } catch (err) {
@@ -30,21 +35,53 @@ function* getMoviesSaga(action) {
   }
 }
 
-// function* postMoviesSaga(action) {
-//   try {
-//     yield axios.post('/movies', action.payload);
-//     // put = this.props.dispatch()
-//     yield put({
-//       type: 'GET_MOVIES', //CALLING THE GET SAGA TO RUN, POST --> GET
-//     });
-//   } catch (err) {
-//     console.log('error in postMoviesSaga');
-//   }
-// }
+function* getMovieSaga(action) {
+  try {
+    const movieId = action.payload;
+    const response = yield axios.get(`/movies/details/${movieId}`),
+    yield put({
+      type: 'SET_DETAILS',
+      payload: response.data [0],
+    })
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+function* putMovieDetailsSaga(action) {
+    try {
+        const movieId = action.payload.id;
+        yield axios.put(`/api/movies/edit/${movieId}`, action.payload);
+        yield put({
+            type: 'GET_MOVIE',
+            payload: movieId,
+        });
+        yield put({
+            type: 'GET_MOVIE_GENRES',
+            payload: movieId,
+        });
+    } catch(err) {
+        console.warn(err);
+    }
+}
+
+function* getMovieGenresSaga(action) {
+    try {
+        const movieId = action.payload;
+        const response = yield axios.get(`/api/movies/genres/${movieId}`);
+        yield put({
+            type: 'SET_GENRES',
+            payload: response.data,
+        })
+    } catch(err) {
+        console.warn(err);
+    }
+}
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
+// REDUCERS
 // Used to store movies returned from the server
 const moviesReducer = (state = [], action) => {
   switch (action.type) {
